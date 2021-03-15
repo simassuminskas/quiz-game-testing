f945d89d9252929d51c402dc9a3f700e91b15570
simassuminskas <78354533+simassuminskas@users.noreply.github.com>
2021-03-15 09:52:22 +0200

    Update card.js

--- public/js/card.js
+++ public/js/card.js
@@ -1,3 +1,4 @@
+var cardStatus = 'front';
 function flip(next, showBtn = true)
 {
   if ((next == 'back') && (userPlay))
@@ -15,9 +16,19 @@ function flip(next, showBtn = true)
         "roomCode" : roomCode
       }));
       document.getElementById('back').innerHTML = '<br><br>' + text + '<br>' + 'SCORE: ' + '<label class="lblScore">' + score + '</label>';
+      document.getElementById('back').innerHTML += '<div id="nextBtnDivArea3" style="padding-left: 90%; padding-bottom: 1%;"></div>';
       nextStep = 'showSpinner';
       document.getElementById('nextBtnDivArea3').style.display = 'block';
       document.getElementById('nextBtnDivArea3').innerHTML = '<i class="fas fa-angle-right fa-2x" onclick="showNextStep();"></i>';
+      cardStatus = 'back';
+    }
+  }
+  else
+  {
+    if (cardStatus != 'front')
+    {
+      $('#card').toggleClass('flipped');
+      cardStatus = 'front';
     }
   }
 }
