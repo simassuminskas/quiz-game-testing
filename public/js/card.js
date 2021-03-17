ef003fb316dfbbd901ef4a5e71fb03e3d1c5f99b
simassuminskas <78354533+simassuminskas@users.noreply.github.com>
2021-03-17 15:34:09 +0200

    Update card.js

--- public/js/card.js
+++ public/js/card.js
@@ -1,31 +1,34 @@
 var cardStatus = 'front';
-function flip(next, showBtn = true)
+var showBtn = false;
+function flip(next = 'back', showBtn = true)
 {
-  if ((next == 'back') && (userPlay))
+  if (userPlay)
   {
-    $('#card').toggleClass('flipped');
-
-    if (showBtn)
+    if ((cardStatus == 'front') && (next == 'back'))
     {
-      socket.emit('area3Card', JSON.stringify({
-        "userName" : userName, 
-        "userSurname" : userSurname, 
-        "teamName" : teamName, 
-        "text" : text, 
-        "score" : score, 
-        "roomCode" : roomCode
-      }));
-      document.getElementById('back').innerHTML = '<br><br>' + text + '<br>' + 'SCORE: ' + '<label class="lblScore">' + score + '</label>';
-      document.getElementById('back').innerHTML += '<div id="nextBtnDivArea3" style="padding-left: 90%; padding-bottom: 1%;"></div>';
-      nextStep = 'showSpinner';
-      document.getElementById('nextBtnDivArea3').style.display = 'block';
-      document.getElementById('nextBtnDivArea3').innerHTML = '<i class="fas fa-angle-right fa-2x" onclick="showNextStep();"></i>';
+      $('#card').toggleClass('flipped');
       cardStatus = 'back';
+
+      if (showBtn)
+      {
+        socket.emit('area3Card', JSON.stringify({
+          "userName" : userName, 
+          "userSurname" : userSurname, 
+          "teamName" : teamName, 
+          "text" : text, 
+          "score" : score, 
+          "roomCode" : roomCode
+        }));
+        document.getElementById('nextBtnDivArea3').style.display = 'block';
+        document.getElementById('backContent').innerHTML = '<br><br>' + text + '<br>' + 'SCORE: ' + '<label class="lblScore">' + score + '</label>';
+        nextStep = 'showSpinner';
+      }
+      else
+      {
+        document.getElementById('nextBtnDivArea3').style.display = 'none';
+      }
     }
-  }
-  else
-  {
-    if (cardStatus != 'front')
+    if ((cardStatus == 'back') && (next == 'front'))
     {
       $('#card').toggleClass('flipped');
       cardStatus = 'front';
