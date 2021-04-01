ca9769a5268f88b1a2ae5d7ad48bc945f009ad8c
simassuminskas <78354533+simassuminskas@users.noreply.github.com>
2021-04-01 11:40:04 +0300

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -435,7 +435,7 @@ function showNextStep()
             document.getElementById('lblLightBoxArea1Header').innerHTML = '';
             document.getElementById('area1Table').style.display = 'none';
             document.getElementById('area1LabelsTable').style.display = 'none';
-            document.getElementById('personalEvaluation').innerHTML = 'EVALUATION<br>ARE WE ACTING LIKE THIS TODAY?<br>compare to the highest scoring response'<br><br> Distant <input type="range" id="personalEvaluationRange" min="0" max="4"> Firm;
+            document.getElementById('personalEvaluation').innerHTML = 'EVALUATION<br>ARE WE ACTING LIKE THIS TODAY?<br>compare to the highest scoring response'<br><br> 'Distant' <input type="range" id="personalEvaluationRange" min="0" max="4"> 'Firm';
             beforeStep = 'showFinalAnswer';
             document.getElementById('beforeBtnDivArea1').innerHTML = '<i class="fas fa-angle-left fa-2x" onclick="showBeforeStep();"></i>';
             document.getElementById('beforeBtnDivArea1').style.display = 'block';
