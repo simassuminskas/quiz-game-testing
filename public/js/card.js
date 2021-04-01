8c37043088751b3a5918ee1ab6040bfaba8ae13d
Muhammad Irfan <irfan@centangle.com>
2021-04-01 10:50:31 +0500

    style changes

--- public/js/card.js
+++ public/js/card.js
@@ -1,39 +1,40 @@
-var cardStatus = 'front';
+var cardStatus = "front";
 var showBtn = false;
-function flip(next = 'back', showBtn = true)
-{
-  if (userPlay)
-  {
-    if ((cardStatus == 'front') && (next == 'back'))
-    {
-      $('#card').toggleClass('flipped');
-      cardStatus = 'back';
+function flip(next = "back", showBtn = true) {
+  if (userPlay) {
+    if (cardStatus == "front" && next == "back") {
+      $("#card").toggleClass("flipped");
+      cardStatus = "back";
 
-      if (showBtn)
-      {
-        socket.emit('area3Card', JSON.stringify({
-          "userName" : userName, 
-          "userSurname" : userSurname, 
-          "teamName" : teamName, 
-          "text" : text, 
-          "score" : score, 
-          "roomCode" : roomCode
-        }));
-        document.getElementById('nextBtnDivArea3').style.display = 'block';
-        document.getElementById('backContent').innerHTML = '<br><br>' + text + '<br>' + 'SCORE: ' + '<label class="lblScore">' + score + '</label>';
-        nextStep = 'showSpinner';
+      if (showBtn) {
+        socket.emit(
+          "area3Card",
+          JSON.stringify({
+            userName: userName,
+            userSurname: userSurname,
+            teamName: teamName,
+            text: text,
+            score: score,
+            roomCode: roomCode,
+          })
+        );
+        document.getElementById("nextBtnDivArea3").style.display = "block";
+        document.getElementById("backContent").innerHTML =
+          "<br><br>" +
+          text +
+          '<div id="score-wrapper"><span>SCORE</span><label class="lblScore">' +
+          score +
+          "</label></div>";
+        nextStep = "showSpinner";
         scoreArea3 += score;
         showGameInfo();
-      }
-      else
-      {
-        document.getElementById('nextBtnDivArea3').style.display = 'none';
+      } else {
+        document.getElementById("nextBtnDivArea3").style.display = "none";
       }
     }
-    if ((cardStatus == 'back') && (next == 'front'))
-    {
-      $('#card').toggleClass('flipped');
-      cardStatus = 'front';
+    if (cardStatus == "back" && next == "front") {
+      $("#card").toggleClass("flipped");
+      cardStatus = "front";
     }
   }
 }
